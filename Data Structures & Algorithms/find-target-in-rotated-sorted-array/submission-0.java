7f1df444177edc2438297ec5df568f8f3fc63e48
Kaviarasu Sakthivadivel <kascatchme@gmail.com>
2026-08-10 16:04:12 -0700

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        // Splitting at mid, always mean one side is sorted.
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] == target) {
+                return mid;
+            }
+
+            // left side is sorted?
+            if(arr[left] <= arr[mid]) {
+                if(arr[left] <= target && target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(arr[mid] < target && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
