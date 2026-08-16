a67a9a397356f849798043667bb3740504df68e7
Kaviarasu Sakthivadivel <kascatchme@gmail.com>
2026-08-16 09:17:41 -0700

    Add: design-hashmap - submission-3

---
+++ Data Structures & Algorithms/design-hashmap/submission-3.java
@@ -0,0 +1,76 @@
+class ListNode {
+    int key, value;
+    ListNode next;
+
+    public ListNode() {
+        this(-1, -1, null);
+    }
+
+    public ListNode(int key, int value, ListNode next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class MyHashMap {
+    private ListNode[] map;
+
+    public MyHashMap() {
+        map = new ListNode[1000];
+        for(int i = 0; i < 1000; i++) {
+            map[i] = new ListNode();
+        }
+    }
+    
+    private int _hash(int key) {
+        return key % this.map.length;
+    }
+
+    public void put(int key, int value) {
+        ListNode current = map[_hash(key)];
+
+        while(current.next != null) {
+            if(current.next.key == key) {
+                current.next.value = value;
+                return;
+            }
+            current = current.next;
+        }
+
+        current.next = new ListNode(key, value, null);
+    }
+    
+    public int get(int key) {
+        ListNode current = map[_hash(key)];
+        while(current != null) {
+            if(current.key == key) {
+                return current.value;
+            }
+
+            current = current.next;
+        }
+
+        return -1;
+    }
+    
+    public void remove(int key) {
+        ListNode current = map[_hash(key)];
+
+        while(current.next != null) {
+            if(current.next.key == key) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
